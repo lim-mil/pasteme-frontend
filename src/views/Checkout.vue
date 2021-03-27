78257de22606bf27c9f2b860db2f3fd4a13507b8
lim-mil <limyel@outlook.com>
2021-03-27 11:43:23 +0800

    github actions

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -5,8 +5,16 @@
 </template>
 
 <script>
+import {apiCheckout} from "@/request/api";
+
 export default {
-name: "Checkout"
+  name: "Checkout",
+  mounted() {
+    apiCheckout(this.$route.params.id).then(response => {
+      localStorage.setItem("JWT_TOKEN", response.data.jwt_token);
+      this.$router.push({"name": "Record"});
+    })
+  }
 }
 </script>
 
